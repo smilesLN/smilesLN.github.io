d6ec2cf950265f04dd2ba7df53ad9241ccf438bc
L8453N <1498879944@qq.com>
2024-06-13 10:20:35 +0800

    Site updated: 2024-06-13 10:20:30

--- js/bibi.js
+++ js/bibi.js
@@ -17,7 +17,7 @@ function getNew() {
         bibi.removeChild(document.getElementById('more'))
     } catch (error) { }
 
-    bibi.innerHTML += '<div id="bb_loading"><img src="https://pic.imgdb.cn/item/661cc56868eb9357137607e9.gif" alt="bb_loading"></div>' // bb_loading图片可以f12在我网站源码下载，也可以使用其他图片。https://pic.imgdb.cn/item/66187efc68eb935713a09acb.gif
+    bibi.innerHTML += '<div id="bb_loading"><img src="/assets/loading3.gif" alt="bb_loading"></div>' // bb_loading图片可以f12在我网站源码下载，也可以使用其他图片。
 
     fetch(Url + page).then(res => res.json()).then((res) => {
         total = res.data.total
